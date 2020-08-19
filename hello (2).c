2814ad5281732bd62a751808e05ce9a8d65bb37c
tbatson3 <69780447+tbatson3@users.noreply.github.com>
2020-08-19 10:38:32 -0500

    Initial commit

---
+++ hello (2).c
@@ -0,0 +1,17 @@
+/**
+ * Author: Trevor Batson
+ *         
+ * Date: 2020/08/17
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Trevor Batson\n");
+
+  return 0;
+}
